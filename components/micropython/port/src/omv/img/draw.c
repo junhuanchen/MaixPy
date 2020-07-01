7907094969030acb1f813264c48bdd118077e7d3
junhuanchen <junhuanchen@qq.com>
2020-07-01 13:53:17 +0800

    fix bmp 24bbp rgb to bgr.

--- components/micropython/port/src/omv/img/draw.c
+++ components/micropython/port/src/omv/img/draw.c
@@ -417,7 +417,7 @@ void imlib_draw_image(image_t *img, image_t *other, int x_off, int y_off, float 
                     uint16_t r = COLOR_RGB565_TO_R8(pixel);
                     uint16_t g = COLOR_RGB565_TO_G8(pixel);
                     uint16_t b = COLOR_RGB565_TO_B8(pixel);
-                    uint16_t c=COLOR_R8_G8_B8_TO_RGB565(b>255?255:b,g>255?255:g, r>255?255:r);
+                    uint16_t c=COLOR_R8_G8_B8_TO_RGB565(r>255?255:r,g>255?255:g, b>255?255:b);
                     imlib_set_pixel(img, x_off + x, y_off + y, c);
                 }
                 else
